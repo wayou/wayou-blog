f811668b5f4f0856f048f9c3185121c2e64b7b93
wayou <liuwayong@gmail.com>
2014-10-21 14:00:35 +0800

    remove menu highlight

--- themes/material/source/js/main.js
+++ themes/material/source/js/main.js
@@ -27,38 +27,5 @@ $(function() {
         }
     });
 
-    // highlight the menu
-    // menuHighlight();
-
 });
 
-//this is adapted from http://css-tricks.com/moving-highlight/
-function menuHighlight() {
-    var originalBG = $(".nav li").css("background-color"),
-        x, y, xy, bgWebKit, bgMoz,
-        lightColor = "rgba(44,108,222,0.9)",
-        gradientSize = 60;
-
-    $('.nav li')
-        .mousemove(function(e) {
-            x = e.pageX - this.offsetLeft;
-            y = e.pageY - this.offsetTop;
-            xy = x + " " + y;
-
-            bgWebKit = "-webkit-gradient(radial, " + xy + ", 0, " + xy + ", " + gradientSize + ", from(" + lightColor + "), to(rgba(66,133,244,1.0))), " + originalBG;
-            bgMoz = "-moz-radial-gradient(" + x + "px " + y + "px 45deg, circle, " + lightColor + " 0%, rgba(66,133,244,1.0) " + gradientSize + "px)";
-
-            $(this)
-                .css({
-                    background: bgWebKit
-                })
-                .css({
-                    background: bgMoz
-                });
-
-        }).mouseleave(function() {
-            $(this).css({
-                background: originalBG
-            });
-        });
-}
