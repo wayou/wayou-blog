22a80958c9b0a8e2d7b2584ef8d4cb76326cc6ec
wayou <liuwayong@gmail.com>
2014-10-21 15:11:10 +0800

    disable backtotop on mobile

--- themes/material/source/js/main.js
+++ themes/material/source/js/main.js
@@ -19,13 +19,14 @@ $(function() {
     }
 
     //show back to top btn on none mobile screen
-    $(window).scroll(function() {
-        if ($(this).scrollTop()) {
-            $('#gotop:hidden').stop(true, true).fadeIn();
-        } else {
-            $('#gotop').stop(true, true).fadeOut();
-        }
-    });
+    if (screen.width > 500) {
+        $(window).scroll(function() {
+            if ($(this).scrollTop()) {
+                $('#gotop:hidden').stop(true, true).fadeIn();
+            } else {
+                $('#gotop').stop(true, true).fadeOut();
+            }
+        });
+    }
 
 });
-
